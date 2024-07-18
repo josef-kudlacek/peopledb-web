c0e9c5da2fd19813f82aa0d626bd2e5fd646dba6
Josef Kudláček <j.k.kudlacek@gmail.com>
2024-07-18 16:05:53 +0200

    245 - Hello world

---
+++ src/test/java/eu/kudljo/peopledbweb/PeopledbWebApplicationTests.java
@@ -0,0 +1,11 @@
+package eu.kudljo.peopledbweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PeopledbWebApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
