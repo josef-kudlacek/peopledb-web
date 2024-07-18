adc9c9619aa2fab3e89f26c58ee640fdb568bdda
Josef Kudláček <j.k.kudlacek@gmail.com>
2024-07-18 17:19:14 +0200

    248 - Bootstrap table for people

--- src/main/java/eu/kudljo/peopledbweb/web/controller/PeopleController.java
+++ src/main/java/eu/kudljo/peopledbweb/web/controller/PeopleController.java
@@ -20,6 +20,8 @@ public class PeopleController {
                 new Person(10L, "Jake", "Snake", LocalDate.of(1950, 1, 1), new BigDecimal("50000")),
                 new Person(20L, "Sarah", "Smith", LocalDate.of(1960, 2, 1), new BigDecimal("60000")),
                 new Person(30L, "Johnny", "Jackson", LocalDate.of(1970, 3, 1), new BigDecimal("70000")),
+                new Person(30L, "Jerry", "McGuire", LocalDate.of(1997, 3, 1), new BigDecimal("70000")),
+                new Person(30L, "Jane", "Smith", LocalDate.of(1970, 3, 1), new BigDecimal("70000")),
                 new Person(40L, "Bobby", "Kim", LocalDate.of(1980, 4, 1), new BigDecimal("80000"))
         );
         model.addAttribute("people", people);
