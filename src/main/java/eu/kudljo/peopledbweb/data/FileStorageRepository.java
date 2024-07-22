de34f759f3acdef364bfdaf0b3cd17dc9e6a1df6
Josef Kudlacek <j.k.kudlacek@gmail.com>
2024-07-22 14:12:17 +0200

    259 - Retrieving uploaded files

--- src/main/java/eu/kudljo/peopledbweb/data/FileStorageRepository.java
+++ src/main/java/eu/kudljo/peopledbweb/data/FileStorageRepository.java
@@ -1,10 +1,13 @@
 package eu.kudljo.peopledbweb.data;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Repository;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -16,10 +19,24 @@ public class FileStorageRepository {
 
     public void save(String originalFilename, InputStream inputStream) {
         try {
-            Path filePath = Path.of(storageFolder).resolve(originalFilename).normalize();
+            Path filePath = getFilePath(originalFilename);
             Files.copy(inputStream, filePath);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public Resource findByName(String filename) {
+        try {
+            Path filePath = getFilePath(filename);
+            return new UrlResource(filePath.toUri());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private Path getFilePath(String filename) {
+        return Path.of(storageFolder).resolve(filename).normalize();
     }
 }
